f370435c5fcb0cf04457b49d4d720fa362b5cf5a
Jon Harper <jon.harper87@gmail.com>
2016-10-12 12:01:55 +0200

    AVATARSRV-10: fix table creation with hsql

--- src/sql/plugins/avatarserver/plugin/create_db_avatarserver.sql
+++ src/sql/plugins/avatarserver/plugin/create_db_avatarserver.sql
@@ -5,12 +5,12 @@
 
 DROP TABLE IF EXISTS avatarserver_avatar;
 CREATE TABLE avatarserver_avatar (		
-id_avatar int(11) NOT NULL default '0',
-email varchar(255) NOT NULL default '',
-mime_type varchar(50) NOT NULL default '',
+id_avatar int default '0' NOT NULL,
+email varchar(255) default '' NOT NULL,
+mime_type varchar(50) default '' NOT NULL,
 file_value LONG VARBINARY,
-hash_email varchar(50) NOT NULL default '',
+hash_email varchar(50) default '' NOT NULL,
 PRIMARY KEY (id_avatar),
-UNIQUE KEY hash_email (hash_email)
+CONSTRAINT hash_email UNIQUE (hash_email)
 );
 
